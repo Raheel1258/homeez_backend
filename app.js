e6b1cdcd324e91db7ba11d6f0f92fe4446cff55e
Raheel1258 <rahilahmad1258@gmail.com>
2021-12-03 15:50:05 +0500

    Add CORS.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/dataBase');
 const routes = require('./routes/index');
 
@@ -6,6 +7,8 @@ const dotenv= require('dotenv');
 dotenv.config();
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 app.use('/api/v1', routes);
 
